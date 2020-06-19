5b47f7a6a06ed6606fd1ede006c421a01f169114
Pratik Priyadarsan <priyadarshanpratik123@gmail.com>
2020-06-20 00:55:30 +0530

    Starting Files from create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Layout from './containers/Layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Fragment>
+        <Layout/>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Layout extends Component{
+    
+    state = {
+        cases : null
+    }
+    componentDidMount(){
+        
+    }
+    render(){
+        return(
+        <h1>Hello</h1>)
+    }
+}
+
+export default Layout
